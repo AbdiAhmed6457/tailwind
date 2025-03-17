3b7f80c49d8f5c5af194168663fd32deeb6bf3c4
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2025-03-17 04:47:45 -0700

    working with react and railwind

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css"; // <-- This should be importing your tailwind file
+
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-10">
+      <h1 className="text-4xl font-bold">Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ my-react-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}" 
+  ],
+  theme: {
+    extend: {}, // You can add custom styles here if needed.
+  },
+  plugins: [], // Add any plugins like Flowbite here if needed.
+}
